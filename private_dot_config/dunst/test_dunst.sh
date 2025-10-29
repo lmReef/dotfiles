9e089a5d0571570c73cf7e658f5ee9d9231f4e02
lmReef <ReefMatson@proton.me>
2025-10-30 11:31:57 +1300

    feat: add tmux, alacritty, doom, dunst, lsd, solaar, waybar

---
+++ private_dot_config/dunst/test_dunst.sh
@@ -0,0 +1,6 @@
+killall dunst
+dunst &
+
+dunstify -u critical "Critical" "Something went wrong."
+dunstify -u normal "Normal" "This is a test notification."
+dunstify -u low "Low" "This is a test notification."
