b55aa77cc6d9fe32ae7a8ec5c8b5993d520713a5
lmReef <ReefMatson@proton.me>
2025-11-17 07:36:30 +1300

    fix: hostname arch

--- fish/conf.d/work-profile.fish
+++ fish/conf.d/work-profile.fish
@@ -1,4 +1,4 @@
-if test is-interactive && test (hostname) = "work"
+if test is-interactive && test (hostnamectl hostname) = work
     set -a PATH \
         ~/.local/bin/rtg-tools \
         ~/.config/tempus-app-manager/bin
