db7a82991164936d0f00d6079c2e54158dc3e8fe
lmReef <ReefMatson@proton.me>
2025-11-26 08:18:08 +1300

    fix: audio swap script

--- private_dot_config/waybar/executable_toggle_audio_device.sh
+++ private_dot_config/waybar/executable_toggle_audio_device.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-speakers="$(wpctl status -k | grep -E "ALC1220 Analog" -m1 | grep -oE "[0-9]+" | head -n1)"
+speakers="$(wpctl status -k | grep -E "ALC[0-9]+ Analog" -m1 | grep -oE "[0-9]+" | head -n1)"
 headset="$(wpctl status -k | grep -E "USB Audio #1" -m2 | tail -n1 | grep -oE "[0-9]+" | head -n1)"
 
-if wpctl status | grep \* | grep -q "$speakers."; then
+if wpctl status | grep "\*" | grep -q "$speakers."; then
     wpctl set-default $headset
 else
     wpctl set-default $speakers
