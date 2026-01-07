f918ab343a0b842a9fe41c513fd3354c379292c2
lmReef <ReefMatson@proton.me>
2026-01-08 07:32:48 +1300

    feat: add swaync, fix hyprland windowrules

--- private_dot_config/dunst/test_dunst.sh
+++
@@ -1,6 +0,0 @@
-killall dunst
-dunst &
-
-dunstify -u critical "Critical" "Something went wrong."
-dunstify -u normal "Normal" "This is a test notification."
-dunstify -u low "Low" "This is a test notification."
