ad6865e8bc611c2eeeb6a8bbb7b020f6deb20d9c
lmReef <ReefMatson@proton.me>
2026-03-17 08:36:04 +1300

    add: cl tl aliases

--- fish/functions/cl.fish
+++ fish/functions/cl.fish
@@ -1,4 +1,7 @@
 function cl --wraps=csvlens --description 'alias cl csvlens'
-  csvlens $argv
-        
+    if string match -rq '^gs://' $argv
+        gcat $argv | csvlens
+    else
+        csvlens $argv
+    end
 end
